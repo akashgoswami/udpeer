84a30a7c3824fd894d9b13a9bbbf59d4bb71ed16
Akash Goswami <akashgoswami@gmail.com>
2017-02-18 23:02:51 +0000

    First offial draft

--- bin.js
+++ bin.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+
+
 var SimplePeer = require('simple-peer');
 var wrtc = require('wrtc');
 var minimist = require('minimist');
@@ -22,21 +25,28 @@ var argv = minimist(process.argv.slice(2), {
 
 if (argv.help){
     
-    console.log("node index.js --init --myName=[Your unique name] --remote=[Peer unique name] --channel=[Channel] --ports=proxy:client");
-    console.log("  -i --init      = should be true only for initiator of the connection (only one of two)");
+    console.log("node index.js --init true --myName=[Your unique name] --remote=[Peer unique name] --channel=[Channel] --ports=proxy:client");
+    console.log("  -i --init      = should be true only for initiator of the connection (only one of the two)");
     console.log("  -m --my        = A unique node name for your machine.");
     console.log("  -r --remote    = A unique node name for your peer. ");    
     console.log("  -c --channel   = The channel where two nodes will communicate");        
-    console.log("  -s --signal    = Your own private signaling URL. https://github.com/mafintosh/signalhub");            
+    console.log("  -s --signal    = Your own private signaling URLs, seperated by comma. e.g. https://url.com,https://url2.com");            
     console.log("  -p --ports     = The local proxy port, followed by the client port");            
-
+    console.log("  -v --version   = Version");            
+    console.log("  -h --help      = print this message");    
+    
     console.log("");            
     console.log("Example. Machine 1");            
-    console.log("node index.js -m alpha -r beta -c letsplay -p 5000,5001");            
+    console.log("node index.js -m alpha -r beta -c letsplay -p 5000:5001");            
     console.log("At Machine 2");            
-    console.log("node index.js -m beta -r alpha -c letsplay -p 6000,6001");                
+    console.log("node index.js -m beta -r alpha -c letsplay -p 6000:6001");                
 
     process.exit(0);
+}
+
+if (argv.version) {
+  console.log(require('./package.json').version)
+  process.exit(0);
 }
 
 
@@ -45,18 +55,20 @@ var initiator = argv.init || false;
 var myName = argv.my ||  'node-1';
 var peerName = argv.remote || 'node-2';
 var channel = argv.channel || 'udpeer-1';
-var signalURL = argv.signal || 'https://signalhub.mafintosh.com';
+var signalURLs = argv.signal || 'https://signalhub.mafintosh.com';
 var ports = (argv.ports || '5555:4444').split(':');
-var localProxyPort = parseInt(ports[0]) || 5555;
-var localPeerPort =  parseInt(ports[1]) || 4444;
+const localhost = '127.0.0.1'; // change to 0.0.0.0 in case your client/server are bound to interfaces
+
+
+var localProxyPort = parseInt(ports[0]);
+var localPeerPort =  parseInt(ports[1]);
 
 var peer = undefined;
 var subscription = undefined;
 var retryHandle = undefined;
 
-
 UDPServer.on('error', function(err) {
-  console.log(`UDP server error:\n${err.stack}`);
+  console.error(`UDP server Unable to bind. error:\n${err.stack}`);
   UDPServer.close();
   process.exit(1);
 });
@@ -64,23 +76,25 @@ UDPServer.on('error', function(err) {
 
 UDPServer.on('listening', () => {
   var address = UDPServer.address();
-  console.log(`UDP server listening at ${address.address}:${address.port}`);
+  console.info(`UDP server listening at ${address.address}:${address.port}`);
   if (peer === undefined)  {
         createPeer();
   }  
 });
 
+
 UDPServer.bind(localProxyPort);
 
-var hub = signalhub(channel, [
-  signalURL
-]);
-
+var hub = signalhub(channel, signalURLs);
 
 UDPServer.on('message', (msg, rinfo) => {
   //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
   localPeerPort = rinfo.port;
-  peer.send(msg);
+  //debug('got a message of length '+ msg.length+ 'from Local port' + localPeerPort);  
+
+  if (connected){
+    peer.send(msg);
+  }
 });
 
 
@@ -108,7 +122,7 @@ function createPeer()
         if (initiator && !connected && retryHandle === undefined){
             retryHandle = setInterval(function(){
                 hub.broadcast(peerName, data);        
-            },15000);
+            },5000);
         }
     })
     
@@ -127,8 +141,8 @@ function createPeer()
     
     peer.on('data', function (data) {
       // got a data channel message
-      //console.log('got a message from peer1: ' + peerName);
-      UDPServer.send( data, 0, data.length, localPeerPort, '0.0.0.0'); 
+      //debug('got a message of length '+ data.length+ 'from peer1: ' + peerName);
+      UDPServer.send( data, 0, data.length, localPeerPort, localhost); 
       
     });
     
