3a5b344cc2cd41bf7532b04219909be05bfef6e6
Akash Goswami <akashgoswami@gmail.com>
2017-02-19 11:34:33 +0000

    first release

--- README.md
+++ README.md
@@ -2,22 +2,32 @@
 
 ## Description
 
-  UDPeer (U-dee-peer) is a simple UDP peer 2 peer communication framework, built upon the hardwork by webrtc and various other awesome Nodejs contributors. 
+UDPeer (you-dee-peer) is a simple UDP peer 2 peer communication framework, built on top of the hardwork done by webrtc developers and various other awesome Nodejs contributors. 
+
   
-  The basic idea is as follows:
-  Currently, nasty firewall rules don't allow software to communicate behind firwalls. However, if two machines could talk via WebRTC, they should also be able to exchange UDP. This is what this project is about.
+**The basic idea is as follows:**
+
+If two machines could talk via WebRTC over NAT and firewalls, they should also be able to exchange UDP packets. This is what this project is all about.
+UDPeer is a simple UDP proxy running on both sides to provide a bidirectional UDP channel.  
   
 ## Prerequisites 
 
-In order to avoid some crashes in webrtc package, following are the prerequisites on Ubuntu/debian packages.
+This proxy will only work if your client and server could communicate over webRTC using public internet. 
+Usually it works without any problem as long as you have outgoing internet connections, but just to verify you could also test this out at various sites such as https://simplewebrtc.com/demo.html
+
+## Installation
+
+Really simple. 
+
+`npm install -g udpeer
 `
-sudo apt-get install python2.7 git-all pkg-config libncurses5-dev libssl-dev libnss3-dev libexpat-dev
-`
+
 ## Overview
+You will need to run seperate instances of udpeer proxy on client and server side. They will establish a communicatîon channel with each other using WebRTC which punches through firewalls. 
+webrtc requirs signalling between nodes, which is done using Signalhub (https://github.com/mafintosh/signalhub) a simple websocket based server. 
+Ideally you should setup your own signalling server and specify the URL there. 
 
-You will need to run seperate instances of udpeer proxy on client and server side. They will establish a communicatîon channel with each other using WebRTC which punches through firewalls. webrtc requirs signalling between nodes, which is done using Signalhub (https://github.com/mafintosh/signalhub) a simple websocket based server. You could run your own public server and specify the URL there. 
-
-Once the proxies establish a communication channel, they will expose a UDP local port on localhost. The client and sever will talk to these local ports instead of talking to each other. 
+Once the proxies establish a communication channel, they will expose a UDP local port on localhost. The client and sever programs will talk to these local ports instead of talking to each other. 
 >```
 >
 >                 +--------+                            +--------+
@@ -37,48 +47,69 @@ Once the proxies establish a communication channel, they will expose a UDP local
 `
 ## How to run
 
-Before you could begin:- 
+Agree upon a unique common channel name between two parties. It is simply a large string. If you are unsure, run UDPeer without a channel name and it will generate one for you. Pass this on to other party. 
+Allocate one proxy UDP port on each of the machine. Let's call it Client proxy port (CProxy) and Server proxy port (SProxy).
 
-##Step1 
+That's it. Install UDPeer on both machines. 
 
-Decide a unique identity for your machine (called ClientIdentity), for the server (ServerIdentity) and the name of common channel (called Ourchannel) where you would exchange messages. The name doesn't need to be very long, but sufficiently unique. 
-
-##Step2
-
-Find which UDP port your client application is bound to and which server port it is using. If your UDP client uses a dynamic port every time, no problem.  The code will automatically update the client port once it connects to us. 
-
-##Step3
-
-Allocate one proxy UDP port on each of the machine. Let's call it Client proxy port (CProxy) and Server proxy port (SProxy)
-
-That's it. 
-
-clone the github repo into a local folder and do. 
-
-`
-npm install .
+`npm install -g udpeer
 `
 
-On local client
-`
-node index.js -i -m ClientIdentity -r ServerIdentity -c Ourchannel -p CProxy:C
+On client side 
+
+`udpeer -c channelId -p CProxy:C
 `
 
 On the server Side
-`
-node index.js -i -m ServerIdentity -r ClientIdentity -c Ourchannel -p SProxy:S
+
+`udpeer -c Ourchannel -p SProxy:S
 `
 
-If everything works well, after a while the server and client proxy shall connect via webrtc and you will see a message e.g.
+Usage
+```
+Usage:
+udpeer [-i] [--channel=channelID] --ports=proxy:client
+  -i --init      = should be true only for initiator of the connection (only one of the two parties)
+  -m --my        = A unique node name for your machine, if you want to override
+  -c --channel   = The channel where two nodes will communicate. Must be unique
+                   If you don't specify, it will be generated, which needs to be shared with your peer.
+  -s --signal    = Your own private signaling URLs, seperated by comma. e.g. https://url.com,https://url2.com
+  -p --ports     = The local ports in the form proxy:local
+  -l --localhost = Localhost IP (127.0.0.1 by default) 
+  -v --version   = Version
+  -h --help      = print this message
 
-`Client connected with  node1  on channel  udpeer`
+Example. Machine 1
+udpeer -cFQBLA7?H8N5ZHP65 -p 5000:5001
+At Machine 2
+udpeer -cFQBLA7?H8N5ZHP65 -p 6000:6001
+```  
+
+If everything works well, after a while the server and client proxy shall connect via webrtc and you will see a message on console. 
 
 ##Modifying client configuration
 
-You could then change your server IP addresses in the client to talk to localhost proxy ports instead of remote Addresses.
+Once the proxy is setup, you could then change your server IP addresses in the client settings to talk to localhost proxy ports instead of remote Addresses.
 
 E.g. if your server configuration in the client was 12.131.121.14:3456
 Then it will become 0.0.0.0:7890 (if you chose 7890 as CProxy in previous example)
 
+*What if my my client doesn't uses fixed source port for communication?
+Don't worry, we have got this covered. As soon as the client connects to proxy, the proxy will save the client source port and reply back to the same next time. 
 
-Note: The current code doesn't encrypt the communication. Please use at your own risk. Please follow any guidelines your IT staff has provided regarding basic port security. 
+**Note: The current code doesn't encrypt the communication. Please use at your own risk. Please follow any guidelines your IT staff has provided regarding basic port security.** 
+
+##Crash Due to  WebRTC
+
+In order to avoid some crashes in webrtc package in Ubuntu, following are the prerequisites on Ubuntu/debian packages. 
+Please note that these are dependencies for Node.js webrtc package, not UDPeer directly. Please give it a try
+`
+sudo apt-get install python2.7 git-all pkg-config libncurses5-dev libssl-dev libnss3-dev libexpat-dev
+`
+
+## Acknowledgement
+
+This package is possible, thanks to the hard work of
+
+https://github.com/mafintosh/signalhub
+https://github.com/js-platform/node-webrtc
--- bin.js
+++ bin.js
@@ -8,41 +8,53 @@ var signalhub = require('signalhub')
 const dgram = require('dgram');
 const UDPServer = dgram.createSocket('udp4');
 var minimist = require('minimist');
-
+var peerName = undefined;
 
 var argv = minimist(process.argv.slice(2), {
     boolean: [ 'init' ],
+    string: [ 'ports' ],    
     alias: {
-        m: 'my',
-        r: 'remote',        
+        i: 'init',
+        m: 'myName',
         c: 'channel',        
         s: 'signal',                
         h: 'help',
+        v: 'version',
+        l: 'localhost',
         p: 'ports'
     }
 });
 
 
-if (argv.help){
-    
-    console.log("node index.js --init true --myName=[Your unique name] --remote=[Peer unique name] --channel=[Channel] --ports=proxy:client");
-    console.log("  -i --init      = should be true only for initiator of the connection (only one of the two)");
-    console.log("  -m --my        = A unique node name for your machine.");
-    console.log("  -r --remote    = A unique node name for your peer. ");    
-    console.log("  -c --channel   = The channel where two nodes will communicate");        
+if (argv.help) printHelp();
+
+function printHelp()
+{
+
+    console.log("UDPeer: Enable bidirectional UDP communication over webrtc");
+    console.log("        Enables client server behind NAT/firewalls to communicate as long as they");    
+    console.log("        could talk over webrtc");    
+
+    console.log("Usage:");        
+    console.log("udpeer [-i] [--channel=channelID] --ports=proxy:client");
+    console.log("  -i --init      = should be true only for initiator of the connection (only one of the two parties)");
+    console.log("  -m --my        = A unique node name for your machine, if you want to override");
+    console.log("  -c --channel   = The channel where two nodes will communicate. Must be unique");        
+    console.log("                   If you don't specify, it will be generated, which needs to be shared with your peer.");            
     console.log("  -s --signal    = Your own private signaling URLs, seperated by comma. e.g. https://url.com,https://url2.com");            
-    console.log("  -p --ports     = The local proxy port, followed by the client port");            
+    console.log("  -p --ports     = The local ports in the form proxy:local");    
+    console.log("  -l --localhost = Localhost IP (127.0.0.1 by default) ");         
     console.log("  -v --version   = Version");            
     console.log("  -h --help      = print this message");    
-    
+    var ch = generator(16, '123456789ABCDEFGHJKLMNOPQRSTUVWXYZ#!?');
     console.log("");            
     console.log("Example. Machine 1");            
-    console.log("node index.js -m alpha -r beta -c letsplay -p 5000:5001");            
+    console.log("udpeer -c"+ch+" -p 5000:5001");            
     console.log("At Machine 2");            
-    console.log("node index.js -m beta -r alpha -c letsplay -p 6000:6001");                
-
+    console.log("udpeer -c"+ch+" -p 6000:6001");                
+    console.log("");       
     process.exit(0);
-}
+};
 
 if (argv.version) {
   console.log(require('./package.json').version)
@@ -50,18 +62,64 @@ if (argv.version) {
 }
 
 
+function generator(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+        
+
 var connected = false;
 var initiator = argv.init || false;
-var myName = argv.my ||  'node-1';
-var peerName = argv.remote || 'node-2';
-var channel = argv.channel || 'udpeer-1';
+// Generate a unique name
+var myName = argv.myName || generator(10+Math.floor(Math.random()*10), '0123456789abcdefghijklmnopqrstuvwxyz');
+var channel = argv.channel || generator(24, '123456789ABCDEFGHJKLMNOPQRSTUVWXYZ#!?');
 var signalURLs = argv.signal || 'https://signalhub.mafintosh.com';
-var ports = (argv.ports || '5555:4444').split(':');
-const localhost = '127.0.0.1'; // change to 0.0.0.0 in case your client/server are bound to interfaces
+var ports = [];
+var localProxyPort = 0;
+var localPeerPort =  0;
+var localhost = argv.localhost || '127.0.0.1'; // change to 0.0.0.0 in case your client/server are bound to interfaces
+
+if (!argv.ports) {
+    console.log("Ports parameter required");
+    printHelp();
+    process.exit(0);
+}
+
+if (argv.ports.indexOf(':') != -1){
+  ports = argv.ports.split(':');  
+  localProxyPort = parseInt(ports[0]);
+  localPeerPort =  parseInt(ports[1]);
+}
+else {
+  localProxyPort = parseInt(argv.ports);
+  localPeerPort = 65533;
+}
+
+if (isNaN(localProxyPort) || isNaN(localPeerPort) || (localProxyPort>65534) || (localPeerPort>65534)){
+    console.log("Error");
+    console.log("Local ports Incorrect. Proxy port", localProxyPort, "local port", localPeerPort);
+    console.log("");
+    printHelp();
+    process.exit(0);
+}
+else
+{
+
+  
+}
 
 
-var localProxyPort = parseInt(ports[0]);
-var localPeerPort =  parseInt(ports[1]);
+if(!argv.channel){
+  console.log("You have not specified any channel. Generating a unique channel");
+  console.log("==============================================================");
+  console.log("Your channel ID:",channel);  
+  console.log("==============================================================");
+  console.log("Please share above channel ID with your peer.\n");    
+}
+
+
 
 var peer = undefined;
 var subscription = undefined;
@@ -76,15 +134,17 @@ UDPServer.on('error', function(err) {
 
 UDPServer.on('listening', () => {
   var address = UDPServer.address();
-  console.info(`UDP server listening at ${address.address}:${address.port}`);
+  console.log("UDP proxy Listening on",address.address+":"+address.port+" -> "+localPeerPort);  
+
   if (peer === undefined)  {
         createPeer();
   }  
 });
 
 
-UDPServer.bind(localProxyPort);
-
+UDPServer.bind(localProxyPort, localhost);
+console.log("Connecting on Channel:", channel);
+console.log("Your Identity:", myName);
 var hub = signalhub(channel, signalURLs);
 
 UDPServer.on('message', (msg, rinfo) => {
@@ -93,42 +153,60 @@ UDPServer.on('message', (msg, rinfo) => {
   //debug('got a message of length '+ msg.length+ 'from Local port' + localPeerPort);  
 
   if (connected){
-    peer.send(msg);
+    try{
+      peer.send(msg);
+    }
+    catch(e)
+    {
+      console.error("Failed to send message to peer. ",e.reason);
+    }
   }
 });
 
 
-
 function createPeer()
 {
-    var subscription = hub.subscribe(myName)
+    var subscription = hub.subscribe("*")
       .on('data', function (message) {
-          if (peer){
-            peer.signal(message);
+          if (peer && message.sender != myName){
+
+            console.log("WebRTC-",message.data.type);
+            if (message.data.type == 'offer' && retryHandle){
+              // Received an offer, can't send offer again. 
+              clearInterval(retryHandle);
+              retryHandle = undefined;
+            }
+            try{
+              peer.signal(message.data);
+            }
+            catch(e){
+              console.error("Unable to signal peer", e.reason);
+            }
+            if (peerName != undefined && peerName != message.sender){
+              console.log("Received a message from",message.sender, "along with",peerName);
+              console.log("Are you sure that only 2 peers are on this channel?");              
+            }
+            peerName = message.sender;
           }
-          if (retryHandle){
-            clearInterval(retryHandle);
-            retryHandle = undefined;
-          }    
     })
 
     peer = new SimplePeer({ initiator: initiator, wrtc: wrtc, trickle: false , objectMode: true, reconnectTimer: true });    
     
-    peer.on('signal', function (data) {
+    peer.on('signal', function (msg) {
         // when peer1 has signaling data, give it to peer2 somehow
         //var signal = Buffer(JSON.stringify(data)).toString('base64');    
-        hub.broadcast(peerName, data);    
+        hub.broadcast("*", { sender: myName, data: msg});    
         
         if (initiator && !connected && retryHandle === undefined){
             retryHandle = setInterval(function(){
-                hub.broadcast(peerName, data);        
-            },5000);
+                hub.broadcast("*", { sender: myName, data: msg});        
+            },10000);
         }
     })
     
     peer.on('connect', function () {
       // wait for 'connect' event before using the data channel
-      console.log("Client connected with ", peerName, " on channel ",channel);
+      console.log("Client connected with", peerName, "on channel",channel);
       connected = true;
       if (retryHandle){
         clearInterval(retryHandle);
@@ -151,6 +229,7 @@ function createPeer()
       connected = false;
       peer.destroy();
       peer = undefined;
+      peerName = undefined;
       createPeer();
     })
 }
